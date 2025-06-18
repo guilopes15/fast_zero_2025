586a2547d7f516ccf36189b6e83b5ab0938ca30b
guilherme <guilherme-lopes58@hotmail.com>
2025-06-17 21:59:52 -0300

    Adicionando schema de Message

--- fast_zero_2025/app.py
+++ fast_zero_2025/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero_2025.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Ola mundo!'}
+
+
+@app.get('/ola', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def ola_mundo():
+    return """
+    <html>
+      <head>
+        <title>Nosso ola mundo!</title>
+      </head>
+      <body>
+        <h1>Ola mundo!</h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,19 @@ def test_root_deve_retornar_ola_mundo():
     response = client.get('/')
     assert response.json() == {'message': 'Ola mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_ola_mundo_deve_retornar_html():
+    response = client.get('/ola')
+    assert (
+        response.text
+        == """
+    <html>
+      <head>
+        <title>Nosso ola mundo!</title>
+      </head>
+      <body>
+        <h1>Ola mundo!</h1>
+      </body>
+    </html>"""
+    )
