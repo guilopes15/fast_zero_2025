bcf25a55bb9eefeb9cb0489de38cbc080d58999f
guilherme <guilherme-lopes58@hotmail.com>
2025-07-23 13:02:52 -0300

    Fazendo deploy

--- fast_zero_2025/database.py
+++ fast_zero_2025/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
+# from sqlalchemy import create_engine
 from fast_zero_2025.settings import Settings
 
 engine = create_async_engine(Settings().DATABASE_URL)
+# engine = create_engine(
+#     Settings().DATABASE_URL,
+#     max_overflow=10,
+#     pool_size=5,
+#     pool_recycle=200,
+# )
 
 
 async def get_session():  # pragma: no cover
